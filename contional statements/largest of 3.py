b42039df0ced8e661514720d5461b83cce6655fa
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-03-22 12:33:52 +0530

    Create largest of 3.py

---
+++ contional statements/largest of 3.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first numeber"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if a>b and a>c:
+ print('largest=',a)
+elif b>a and b>c:
+ print('largest=',b)
+else :
+  print('largest=',b)
