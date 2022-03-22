27c5d9102ebf516bd972d000373f0b2e74968723
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-03-22 12:48:27 +0530

    Create week.py

---
+++ contional statements/week.py
@@ -0,0 +1,16 @@
+day=int(input("enter a number between 1 to 7"))
+if day==1:
+  print("Mon")
+elif day==2:
+  print("tue")
+elif day==3:
+  print("Wed")
+elif day==4:
+  print("Thu")
+elif day==5:
+  print("Fri")
+elif day==6:
+  print("Sat")
+else :
+  print("Sun")
+  
