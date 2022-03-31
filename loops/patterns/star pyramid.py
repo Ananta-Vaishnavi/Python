31b64c8bf20092daa7ffb86acca492b05d2dca23
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-03-31 12:31:53 +0530

    Update star pyramid.py

--- loops/patterns/star pyramid.py
+++ loops/patterns/star pyramid.py
@@ -1,3 +1,12 @@
+'''
+n=int(input("Enter a number  "))
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print(i,end=" ")
+  print('\n')
+  
+'''
+
 n=int(input("Enter a number  "))
 for i in range(1,n+1):
   for j in range(1,i):
