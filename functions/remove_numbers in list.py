32ee933b9e5ddf85f787c1349ccad4fbaf62e10f
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-04-03 12:43:01 +0530

    Create remove_numbers in list.py

---
+++ functions/remove_numbers in list.py
@@ -0,0 +1,13 @@
+cart = [ 50,60,800,700,10]
+c=0
+for num in cart:
+        if (num>500):
+            c+=1
+def remove(cart):
+    for i in range(len(cart)-1):
+        if (cart[i]>500):
+            cart.pop(i)
+            break
+for i in range(0,c):
+    remove(cart)
+print(cart)
