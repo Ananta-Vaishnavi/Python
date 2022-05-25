f1fc4049b22622d1c62c9019951dc1c595580b3b
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-05-25 22:07:37 +0530

    Update list to nested dictionary{1: {2: {3: {4: {}}}}} .py

--- data types/list to nested dictionary{1: {2: {3: {4: {}}}}} .py
+++ data types/list to nested dictionary{1: {2: {3: {4: {}}}}} .py
@@ -1,7 +1,16 @@
-def list_nd(l):
-  d1= d=dict()
-  for i in l:
-    d[i]=dict()
-    d=d[i]
-  return d1
-print(list_nd([1,2,3,4]))
+# def list_nd(l):
+#   d1= d=dict()
+#   for i in l:
+#     d[i]=dict()
+#     d=d[i]
+#   return d1
+# print(list_nd([1,2,3,4]))
+def key(list):
+    if len(list) == 1:
+        return {list[0]: {}}
+    else:
+        return {list[0]: key(list[1:])}
+
+
+print(key([1, 2, 3, 4]))
+    
