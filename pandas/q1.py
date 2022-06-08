ac5620c2a247a8e7aa3e22ec744a9c89714b5903
Ananta Vaishnavi Movva <94735204+Ananta-Vaishnavi@users.noreply.github.com>
2022-06-08 10:07:42 +0530

    Update and rename q1.py to series.py

--- pandas/q1.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-ds= pd.Series([1,2,3,4,5,6,7])
-print(ds)
---
+++ pandas/series.py
@@ -0,0 +1,4 @@
+#1) Write a Pandas program to create and display a one-dimensional array-like object containing an array of data using Pandas module.
+import pandas as pd
+ds= pd.Series([1,2,3,4,5,6,7])
+print(ds)
